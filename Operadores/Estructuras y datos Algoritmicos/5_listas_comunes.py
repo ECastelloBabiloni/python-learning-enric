4a315b784b8185c841b7a9e69f19ac42983b3b72
Enric <ecastellobabiloni@gmail.com>
2026-03-18 10:05:31 +0100

    subo ejercicio terminado

--- Operadores/Estructuras y datos Algoritmicos/5_listas_comunes.py
+++ Operadores/Estructuras y datos Algoritmicos/5_listas_comunes.py
@@ -1,10 +1,11 @@
 def elementos_comunes(lista_a, lista_b):
-    if not lista_a or lista_b:
+    if not lista_a or not lista_b:
         return "No hay lista"
     comunes = []
     for numero in lista_a:
-       if numero in lista_b:
+        if numero in lista_b and numero not in comunes:
            comunes.append(numero)
+    return comunes
 
 lista_a = [1, 60, 50, 5, 5, 60, 10, 2, 24, 55, 2]
 lista_b = [ 7, 8, 10, 50, 5, 18, 24] 
