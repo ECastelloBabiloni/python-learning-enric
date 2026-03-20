0b17383283386c52dcb5745d75b16a06ab445e57
Enric Castello <rixocb@gmail.com>
2026-03-20 20:10:31 +0100

    subo ejercicio de bucles anidados

--- Operadores/Estructuras y datos Algoritmicos/Bucles_Anidados/8_duplicados.py
+++ Operadores/Estructuras y datos Algoritmicos/Bucles_Anidados/8_duplicados.py
@@ -1,13 +1,12 @@
 def hay_duplicados(lista):
     if len(lista) < 2:
         return "No hay lista"
-    for i in lista:
-        for j in lista:
-            if i == j :
-                continue
+    for i in range(len(lista)-1):
+        for j in range(i+1,len(lista)):
+            
             if lista[j] == lista[i]:
                 return True
     return False
             
-lista= [1, 2, 5, 1, 3]
+lista= [1, 2, 5, 3, 3]
 print(hay_duplicados(lista))
