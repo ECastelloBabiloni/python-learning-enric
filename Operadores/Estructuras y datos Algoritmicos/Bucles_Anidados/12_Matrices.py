29bedc72bdc79bc9b39ffdf3ba9a3004ee5c45e7
Enric Castello <rixocb@gmail.com>
2026-05-29 19:05:37 +0200

    Matrices sumando columnas

--- Operadores/Estructuras y datos Algoritmicos/Bucles_Anidados/12_Matrices.py
+++ Operadores/Estructuras y datos Algoritmicos/Bucles_Anidados/12_Matrices.py
@@ -8,7 +8,32 @@ def sumar_matriz(matriz):
         for numero in fila:
             total_suma += numero
     return total_suma
+
+def sumar_filas(matriz):
+    resultados = []
+    for fila in matriz:
+        suma_fila = 0
+
+        for numero in fila:
+            suma_fila += numero
+
+        resultados.append(suma_fila)
+    return resultados
+
+def sumar_columnas(matriz):
     
+    suma_col_0 = 0
+    suma_col_1 = 0
+    suma_col_2 = 0
+
+    for fila in matriz:
+           
+        suma_col_0 += fila[0]      
+        suma_col_1 += fila[1]
+        suma_col_2 += fila[2]
+   
+    return [suma_col_0, suma_col_1, suma_col_2]
+
 
 matriz = [
     [1, 2, 3],
@@ -18,5 +43,6 @@ matriz = [
 
     
 mostrar_matriz(matriz)
-print(sumar_matriz(matriz))
-
+print("sumar matriz",sumar_matriz(matriz))
+print("sumar filas",sumar_filas(matriz))
+print("sumar columnas", sumar_columnas(matriz))
