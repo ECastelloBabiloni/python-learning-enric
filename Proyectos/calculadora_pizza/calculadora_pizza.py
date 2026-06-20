fce9202a6d79ff38259d4d663eb5838565abbd60
Enric Castello <rixocb@gmail.com>
2026-06-20 12:16:04 +0200

    comenzamos el nuevo proyecto v1

--- Proyectos/calculadora_pizza/calculadora_pizza.py
+++ Proyectos/calculadora_pizza/calculadora_pizza.py
@@ -67,3 +67,23 @@ def calcular_receta_desde_total(receta):
     return masa
 
 print("diccionario masa: ", calcular_receta_desde_total(pizza))
+
+# A partir de aqui pruebo de hacer V1
+
+peso_por_persona = 320
+porcentaje_sal = 2.4
+
+comensales = int(input("Numero de comensales: "))
+hidratacion = int(input("Hidratacion: "))
+
+peso_total = peso_por_persona * comensales
+
+
+print(peso_total)
+
+
+receta_usuario ={
+    "peso_total": peso_total,
+    "hidratacion": hidratacion,
+    "porcentaje_sal": porcentaje_sal
+}
